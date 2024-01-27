0fa89baea1ce85d5e0ae12447fa764f5c1d1b54f
frisko-sposad <safiko@ya.ru>
2024-01-27 23:16:38 +0300

    fix table

--- app/stats_feods/page.tsx
+++ app/stats_feods/page.tsx
@@ -8,35 +8,43 @@ const StatsTable = () => {
   const dataFeods = useMemo(() => polygonsData, []);
 
   const data = dataFeods.map((el) => {
-    const populationLimit =
+    const population =
       el.info.resources.mines +
       el.info.resources.forest +
       el.info.resources.skins +
-      el.info.resources.horses;
+      el.info.resources.horses +
+      el.info.resources.food;
+    const populationLimit =
+      el.info.limits.mines +
+      el.info.limits.forest +
+      el.info.limits.skins +
+      el.info.limits.horses +
+      el.info.limits.food;
+
     const res = {
       id: el.id,
       name: el.info.name,
       text: el.info.text,
-      mines: `${el.info.resources.mines}/${el.info.limits.mines}`,
-      forest: `${el.info.resources.forest}/${el.info.limits.forest}`,
-      skins: `${el.info.resources.skins}/${el.info.limits.skins}`,
-      horses: `${el.info.resources.horses}/${el.info.limits.horses}`,
-      food: `${el.info.resources.food}/${el.info.limits.food}`,
+      // mines: `${el.info.resources.mines}/${el.info.limits.mines}`,
+      // forest: `${el.info.resources.forest}/${el.info.limits.forest}`,
+      // skins: `${el.info.resources.skins}/${el.info.limits.skins}`,
+      // horses: `${el.info.resources.horses}/${el.info.limits.horses}`,
+      // food: `${el.info.resources.food}/${el.info.limits.food}`,
+      mines_resources: el.info.resources.mines,
+      mines_limits: el.info.limits.mines,
+      forest_resources: el.info.resources.forest,
+      forest_limits: el.info.limits.forest,
+      skins_resources: el.info.resources.skins,
+      skins_limits: el.info.limits.skins,
+      horses_resources: el.info.resources.horses,
+      horses_limits: el.info.limits.horses,
+      food_resources: el.info.resources.food,
+      food_limits: el.info.limits.food,
       owner: el.info.owner,
       religion: el.info.religion,
-      populationLimit: `${
-        el.info.resources.mines +
-        el.info.resources.forest +
-        el.info.resources.skins +
-        el.info.resources.horses +
-        el.info.resources.food
-      }/${
-        el.info.limits.mines +
-        el.info.limits.forest +
-        el.info.limits.skins +
-        el.info.limits.horses +
-        el.info.limits.food
-      }`,
+      // populationLimit: `${population}/${populationLimit}`,
+      population,
+      populationLimit,
       overlord: el.info.overlord,
     };
 
@@ -54,34 +62,99 @@ const StatsTable = () => {
         accessor: 'name',
       },
       {
-        Header: 'Работают / Лимит ресурсов',
+        Header: 'Лес',
         columns: [
           {
-            Header: 'Шахты',
-            accessor: 'mines',
+            Header: 'Рабочие',
+            accessor: 'forest_resources',
           },
           {
-            Header: 'Лес',
-            accessor: 'forest',
+            Header: 'Лимит',
+            accessor: 'forest_limits',
+          },
+        ],
+      },
+      {
+        Header: 'Скот',
+        columns: [
+          {
+            Header: 'Рабочие',
+            accessor: 'skins_resources',
           },
           {
-            Header: 'Скот',
-            accessor: 'skins',
+            Header: 'Лимит',
+            accessor: 'skins_limits',
+          },
+        ],
+      },
+      {
+        Header: 'Лошади',
+        columns: [
+          {
+            Header: 'Рабочие',
+            accessor: 'horses_resources',
           },
           {
-            Header: 'Лошади',
-            accessor: 'horses',
+            Header: 'Лимит',
+            accessor: 'horses_limits',
+          },
+        ],
+      },
+      {
+        Header: 'Снабжение',
+        columns: [
+          {
+            Header: 'Рабочие',
+            accessor: 'food_resources',
           },
           {
-            Header: 'Снабжение',
-            accessor: 'food',
+            Header: 'Лимит',
+            accessor: 'food_limits',
+          },
+        ],
+      },
+      {
+        Header: 'Население',
+        columns: [
+          {
+            Header: 'Рабочие',
+            accessor: 'population',
           },
           {
-            Header: 'Население',
+            Header: 'Лимит',
             accessor: 'populationLimit',
           },
         ],
       },
+      // {
+      //   Header: 'Работают / Лимит ресурсов',
+      //   columns: [
+      //     {
+      //       Header: 'Шахты',
+      //       accessor: 'mines',
+      //     },
+      //     {
+      //       Header: 'Лес',
+      //       accessor: 'forest',
+      //     },
+      //     {
+      //       Header: 'Скот',
+      //       accessor: 'skins',
+      //     },
+      //     {
+      //       Header: 'Лошади',
+      //       accessor: 'horses',
+      //     },
+      //     {
+      //       Header: 'Снабжение',
+      //       accessor: 'food',
+      //     },
+      //     {
+      //       Header: 'Население',
+      //       accessor: 'populationLimit',
+      //     },
+      //   ],
+      // },
 
       {
         Header: 'Религия',
