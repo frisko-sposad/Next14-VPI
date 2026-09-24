41e140868a1816a1578f924df19f6f62ec938806
frisko-sposad <safiko@ya.ru>
2026-09-25 00:10:37 +0300

    fix result buttle

--- components/Calculator/flank-results.tsx
+++ components/Calculator/flank-results.tsx
@@ -18,29 +18,61 @@ const FlankResults = ({ flank }: FlankResults) => {
       );
     } else {
       return (
-        <div key={`${squad.squadUnit.id}-${index}`}>
-          <span key={`${index}${squad.squadUnit.name}`} className="px-2">
+        <div key={`${squad.squadUnit.id}-${index}`} className="flex flex-wrap">
+          <span key={`${index}${squad.squadUnit.name}`} className="px-2 w-48">
             {squad.squadUnit.name}
           </span>
+          <span className="text-white">-</span>
           <span
             key={`${index}${squad.squadUnit.squadNumber}start`}
-            className="px-2 "
+            className="px-2 w-12 text-right"
           >
-            {squad.squadUnit.squadNumber}
+            {Math.round(squad.squadUnit.squadNumber)}
           </span>
+          <span className="text-white">-</span>
           <span
             key={`${index}${squad.squadUnit.squadAlive}died`}
-            className="px-2 text-green-600"
+            className="px-2 text-green-600 w-12 text-right"
           >
-            {squad.squadUnit.squadAlive}
+            {Math.round(squad.squadUnit.squadAlive)}
           </span>
+          <span className="text-white">-</span>
           <span
             key={`${index}${squad.squadUnit.squadLosses}survived`}
-            className="px-2 text-red-600"
+            className="px-2 text-red-600 w-12 text-right"
           >
-            {squad.squadUnit.squadLosses}
+            {Math.round(squad.squadUnit.squadLosses)}
           </span>
         </div>
+        // <div key={`${squad.squadUnit.id}-${index}`}>
+        //   <span
+        //     key={`${index}${squad.squadUnit.name}`}
+        //     className="px-2 block w-32"
+        //   >
+        //     {squad.squadUnit.name}
+        //   </span>
+        //   <span className="text-white">-</span>
+        //   <span
+        //     key={`${index}${squad.squadUnit.squadNumber}start`}
+        //     className="px-2 "
+        //   >
+        //     {squad.squadUnit.squadNumber}
+        //   </span>
+        //   <span className="text-white">-</span>
+        //   <span
+        //     key={`${index}${squad.squadUnit.squadAlive}died`}
+        //     className="px-2 text-green-600"
+        //   >
+        //     {squad.squadUnit.squadAlive}
+        //   </span>
+        //   <span className="text-white">-</span>
+        //   <span
+        //     key={`${index}${squad.squadUnit.squadLosses}survived`}
+        //     className="px-2 text-red-600"
+        //   >
+        //     {squad.squadUnit.squadLosses}
+        //   </span>
+        // </div>
       );
     }
   });
